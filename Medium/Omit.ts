27ece9bbc8554c3e127872600f8f378fe7d9a919
YangGwangSeong <rhkdtjd_12@naver.com>
2024-07-20 07:15:56 -0700

    Medium/Omit

---
+++ Medium/Omit.ts
@@ -0,0 +1,27 @@
+import { Equal, Expect } from "../test-utils";
+
+type cases = [
+  Expect<Equal<Expected1, MyOmit<Todo, "description">>>,
+  Expect<Equal<Expected2, MyOmit<Todo, "description" | "completed">>>
+];
+
+// @ts-expect-error
+type error = MyOmit<Todo, "description" | "invalid">;
+
+interface Todo {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+interface Expected1 {
+  title: string;
+  completed: boolean;
+}
+
+interface Expected2 {
+  title: string;
+}
+
+// ============= Your Code Here =============
+type MyOmit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
