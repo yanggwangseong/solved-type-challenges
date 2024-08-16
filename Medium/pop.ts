c8e2317bec1ca29776e023703a71c262549be527
YangGwangSeong <rhkdtjd_12@naver.com>
2024-08-16 01:42:17 -0700

    Medium/pop

---
+++ Medium/pop.ts
@@ -0,0 +1,10 @@
+import { Equal, Expect } from "../test-utils";
+
+type cases = [
+  Expect<Equal<Pop<[3, 2, 1]>, [3, 2]>>,
+  Expect<Equal<Pop<["a", "b", "c", "d"]>, ["a", "b", "c"]>>,
+  Expect<Equal<Pop<[]>, []>>
+];
+
+// ============= Your Code Here =============
+type Pop<T extends any[]> = T extends [...infer Head, infer Tail] ? Head : [];
